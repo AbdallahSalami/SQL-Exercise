e79d49c61b5ae84aef8c80121c4a668a82c3c201
AbdallahSalami <AbdallahSalami@users.noreply.github.com>
2023-09-18 11:29:19 +0300

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,21 @@
+SELECT * FROM students ;
+
+1- SELECT Name FROM students;
+
+2- SELECT * FROM students WHERE Age > 30;
+
+3- SELECT Name FROM students WHERE Age = 30 AND Gender = 'F';
+
+4- SELECT Points FROM students WHERE Name = 'Alex';
+
+5- INSERT INTO students VALUES ( 7, "Abdallah" , 25 , "M" , 1000 ) ;
+
+SELECT * FROM students ;
+
+6- UPDATE students SET Points = Points + 50  WHERE Name = 'Basma';
+
+SELECT * FROM students;
+
+7- UPDATE students SET Points = Points - 4 WHERE Name = 'Alex';
+
+SELECT * FROM students ;  
